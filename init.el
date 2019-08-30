c954324f1552c5ec20a7ec6a79d20d118bd8d91f
Tran Tam <tam.tran@employmenthero.com>
2019-08-30 22:44:18 +0700

    add tmux-emacs symless navigate

--- init.el
+++ init.el
@@ -104,9 +104,11 @@ There are two things you can do about this warning:
 (setq w-dotfiles-folder-path (w/dotfiles-folder-path))
 (setq w-dotfiles-pakages-folder-path (format "%s/packages" w-dotfiles-folder-path))
 (setq evil-evilified-state-path (format "%s/packages/evil-evilified-state.el" w-dotfiles-folder-path))
-(load  (format "%s/packages/yaml-imenu/yaml-imenu.el" w-dotfiles-folder-path))
+(load (format "%s/packages/yaml-imenu/yaml-imenu.el" w-dotfiles-folder-path))
 (load (format "%s/packages/xclip.el" w-dotfiles-folder-path))
+(load (format "%s/packages/navigate.el" w-dotfiles-folder-path))
 (xclip-mode 1)
+(require 'navigate)
 
 ;; This is only needed once, near the top of the file
 (eval-when-compile
